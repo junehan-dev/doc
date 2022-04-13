3d28bfb019bbdaeb1538af90910649547ca3f618
junehan <junehan.dev@gmail.com>
2022-04-13 13:43:12 +0900

    REBUILD

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-#import os
-#import sys
-#sys.path.insert(0, os.path.abspath("."))
+import os
+import sys
+sys.path.insert(0, os.path.abspath("../.."))
 
 
 # -- Project information -----------------------------------------------------
@@ -35,7 +35,7 @@ extensions = [ "sphinx.ext.intersphinx"
     ,"sphinx.ext.imgconverter"
     ,"sphinx.ext.todo"
     ,"sphinx.ext.viewcode"
-#    ,"sphinx.ext."
+    ,"sphinx.ext.githubpages"
 ];
 
 # Add any paths that contain templates here, relative to this directory.
@@ -44,7 +44,7 @@ templates_path = ["_templates"];
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = [];
+exclude_patterns = ["_build"];
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -55,7 +55,7 @@ exclude_patterns = [];
 html_theme = "sphinx_rtd_theme";
 
 # Add any paths that contain custom static files (such as style sheets) here,
-html_static_path = [];
+html_static_path = ["_static"];
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 todo_include_todos = True;
